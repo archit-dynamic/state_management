2174a5c53c8a06eade7c3d1c323e678d0555adb8
archit-dynamic <architdynamic@gmail.com>
2023-10-28 19:05:18 +0530

    inherited widget step-2

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = "Tap the screen";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: const Text("Home Page"),
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        },
+        child: Container(
+          color: Colors.white,
+        ),
       ),
     );
   }
